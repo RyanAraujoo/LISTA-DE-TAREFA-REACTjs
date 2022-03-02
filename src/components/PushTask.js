24db14253040b818d51071b8d53fd9719d74e56c
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-03-02 00:50:42 -0300

    atualização da aplicação

---
+++ src/components/PushTask.js
@@ -0,0 +1,59 @@
+import { Input, Button} from 'reactstrap'
+import { useState } from 'react'
+import RenderTask from './RenderTask'
+export default function PushTask() {
+  // 1 renderizar um "input" de texto
+  // 2 botão renderiza o texto do "input" na lista
+  // 3 renderizar a lista de tarefas (RenderTask.jsx)
+  // 4 adicionar funções de edição,remoção e confirmação da tarefa
+  // 5 identificação da tarefa via propriedades --> objeto
+
+  const [pushTask, setPushTask] = useState('') // pegar o texto do "input"
+  const [Task, setTask] = useState([]) // enviar a texto para a lista de texto
+  
+
+  // onChange do "Input" // setPushTask()
+  const PushTextInput = (value) => {
+   setPushTask(value.target.value)
+  }
+
+  // onClick do "Button" // setTask()
+  const SendTextTask = ()=> {
+    const objTask = {
+    task: pushTask,
+    isComplete: false
+   }
+      return setTask([...Task, objTask])
+  }
+
+  // Função de confirmar a tarefa
+
+  const ConfirmTaskList = index => {
+    const newArray = [...Task]
+    newArray[index].isComplete = !newArray[index].isComplete
+      return setTask(newArray)
+  }
+
+  // Função de remover a tarefa
+  
+  const RemoveTaskList = index => {
+    const newArray = [...Task]
+      newArray.splice(index,1)
+      return setTask(newArray)
+  }
+  return (
+    <div className='App'>
+      <Input onChange={PushTextInput} />
+      <Button
+        active
+        color='success'
+        onClick={SendTextTask}
+      >Enviar</Button>
+       <RenderTask
+       ConfirmTaskList={index => ConfirmTaskList(index)} 
+       RemoveTaskList={index => RemoveTaskList(index)} 
+       Task={Task}
+       />
+    </div>
+  )
+} 
---
+++ src/components/RenderTask.js
@@ -0,0 +1,33 @@
+import { BsFillXOctagonFill,BsFillFileCheckFill} from 'react-icons/bs'
+import {Button} from 'reactstrap'
+
+export default function RenderTask (props) {
+    // "push" da lista via props para ser renderizada
+        const Task = props.Task 
+    return (
+      <div className='App'>
+        <ul className='task-list'>
+            {Task.map((task, index) => {
+          return (  
+            <li key={index} className={task.isComplete ? 'confirme':'no-confirme'}>
+            
+            <Button active color='primary' className='btn-function'>
+            <BsFillFileCheckFill onClick={() => {
+                props.ConfirmTaskList(index) // "push" da função via props // comando
+            }}/>
+            </Button>
+              <div className='txt-task'>
+                  {task.task} 
+              </div>
+                <Button active color='danger' className='btn-function'>
+                  <BsFillXOctagonFill onClick={() => {
+                      props.RemoveTaskList(index) // "push" da função via props // comando
+                  }}/>
+                </Button>
+            </li>
+          )
+      })}
+        </ul>
+        </div>
+    )
+}
